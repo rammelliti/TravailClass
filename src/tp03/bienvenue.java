45987f4983941cc7021fbce011fef52f7b2575aa
Melliti ramla <ram.melliti@gmail.com>
2020-01-14 16:53:34 +0100

    version 14/01

---
+++ src/tp03/bienvenue.java
@@ -0,0 +1,20 @@
+
+package tp03;
+
+public class bienvenue {
+
+    public static void main(String[] args) {
+        afficheBanniere();
+    }
+    public static void afficheBanniere()
+                {
+                    System.out.println("*************************");
+                    System.out.println("**                     **");
+                    System.out.println("**       Bienvenue     **");
+                    System.out.println("**                     **");
+                    System.out.println("*************************");
+                }
+        
+    }
+    
+
